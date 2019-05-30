dc1ea35f858e4c2d0baa4633fde536617b0140cb
yth0 <ythyth0@gmail.com>
2019-05-30 12:38:50 +0900

    qweqwe

--- project/urls.py
+++ project/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('portfolio/', include('portfolio.urls')),
     path('admin/', admin.site.urls),     
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
